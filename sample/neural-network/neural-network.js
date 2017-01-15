e32af0e113d8aa7e64183bfc0cc06a6e1b4fbad8
Arzhan "kai" Kinzhalin <arzhan@kinzhalin.com>
2017-01-15 00:00:39 -0800

    Read and visualize the data.
    Digits are visualized transposed, to be fixed.

--- sample/neural-network/neural-network.js
+++ sample/neural-network/neural-network.js
@@ -1,8 +1,59 @@
+/*eslint indent: [ "error", 4, { "outerIIFEBody": 0 }]*/
+/*eslint-env browser*/
 /*global Matrix: false */
 (function () {
-    /* initialization */
-    function initTheta(outLayerSize, inLayerSize) {
-        var epsilon = 0.12;
-        return Matrix.rand(outLayerSize, inLayerSize+1).x(2*epsilon).sub(epsilon);
-    }
+
+var ts;
+
+/* initialization */
+function initTheta(outLayerSize, inLayerSize) {
+    var epsilon = 0.12;
+    return Matrix.rand(outLayerSize, inLayerSize+1).x(2*epsilon).sub(epsilon);
+}
+
+function getData(url) {
+    return new Promise(function (resolve, reject) { // eslint-disable-line no-unused-vars
+        var req = new XMLHttpRequest();
+        req.overrideMimeType("application/json; charset=utf-8");
+        req.onload = function () {
+            var te,
+                data;
+
+            if (this.response instanceof Object) {
+                data = this.response;
+            } else {
+                throw new Error("Incorrect data format: " + this.response);
+            }
+
+            te = new Date().getTime();
+
+            resolve(data);
+        };
+        req.open("GET", url, true);
+        req.responseType = "json";
+        req.send();
+    });
+}
+
+window.addEventListener("load", function () {
+    var te;
+    ts = (new Date()).getTime();
+
+    getData('./data-samples.json')
+        .then(function (data) {
+            var i = 0;
+            te = (new Date).getTime();
+            function drawNext() {
+                if (i === data.length) return;
+                drawData(data[i]);
+                i++;
+                setTimeout(function () {
+                    drawNext();
+                }, 50);
+            }
+            drawNext();
+        });
+
+});
+
 }());
